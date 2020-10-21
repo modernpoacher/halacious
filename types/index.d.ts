7b21ffeac4b5135c5776d020ccaa8e0206c7338e
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-10-21 11:52:04 +0100

    Types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,24 @@
+declare namespace halacious {
+  export interface Representation {
+    request: {
+      route: {
+        method?: string
+        path?: string
+      }
+      params: {
+        collection?: string
+        member?: string
+      }
+    }
+    entity: {
+      collections?: Array<{ uid: string }>
+      members?: Array<{ uid: string }>
+    }
+    ignore: (v: string) => void
+    embed: (v: string, u: string, m: object) => void
+  }
+}
+
 declare module '@modernpoacher/halacious' {
   export const plugin: object
 }
