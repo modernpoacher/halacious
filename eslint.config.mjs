0cf5a0eb408d65900690421dc3aec61969303dbd
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-03-23 21:51:07 +0000

    ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import globals from 'globals'
+import standard from '@sequencemedia/eslint-config-standard/configs/recommended/merge'
+import typescript from '@sequencemedia/eslint-config-typescript/configs/recommended/merge'
+
+export default [
+  /**
+   *  Standard config
+   */
+  standard({
+    files: [
+      '**/*.{mjs,cjs,mts,cts}'
+    ],
+    ignores: [
+      'src',
+      'test'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  }),
+  /**
+   *  Standard config for `mjs` and `mts` files
+   */
+  standard({
+    files: [
+      'src/**/*.{mjs,cjs,mts,cts}'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  }),
+  /**
+   *  Standard config for `mjs` and `mts` files
+   */
+  standard({
+    files: [
+      'test/**/*.{mjs,cjs,mts,cts}'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.mocha
+      }
+    }
+  }),
+  /**
+   *  TypeScript config
+   */
+  typescript({
+    files: [
+      '**/*.{mts,cts}'
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-use-before-define': 'off'
+    }
+  })
+]
